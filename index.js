4e2af004b1e6879b9c9ed59f7ed9b758403f3f4e
NathanaelAndrade <134087107+NathanaelAndrade@users.noreply.github.com>
2023-11-20 10:15:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function sim() {
+    // Adiciona um ouvinte de evento ao botão
+    document.getElementById("btn").addEventListener("click", function() {
+        // Redireciona para outra página
+        window.location.href = "https://nathanaelandrade.github.io/Josias/";
+    });
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
